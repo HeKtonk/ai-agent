3e046fab70d6cc3fdf6fd06656753d52651a1f6f
HeKtonk <hoyez.corey@gmail.com>
2025-12-06 18:38:15 +0100

    add user input for the prompt as a command line arg

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,10 +7,10 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 def main():
-    print("Hello from ai-agent!")
-     
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
+
     if api_key is None:
         raise RuntimeError("invalid api key")
     client = genai.Client(api_key=api_key)
@@ -17,7 +18,11 @@ def main():
     model = "gemini-2.5-flash"
     prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
-    res = client.models.generate_content(model=model, contents=prompt)
+    parser = argparse.ArgumentParser(description="Ai-chatbot")
+    parser.add_argument("prompt", type=str, help="Write your prompt")
+    args = parser.parse_args()
+
+    res = client.models.generate_content(model=model, contents=args.prompt)
 
 
     if res.usage_metadata is None:
