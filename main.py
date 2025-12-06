f68f8b7e4321276fff2934f33d15e8548aa44c6c
HeKtonk <hoyez.corey@gmail.com>
2025-12-06 18:10:15 +0100

    update main.py to load env and make first request

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
 def main():
     print("Hello from ai-agent!")
+     
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("invalid api key")
+    client = genai.Client(api_key=api_key)
+
+    model = "gemini-2.5-flash"
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+    res = client.models.generate_content(model=model, contents=prompt)
+    print(res.text)
 
 
 if __name__ == "__main__":
